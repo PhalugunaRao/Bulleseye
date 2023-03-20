4bd4a01bb096454a8f01bdc8b56189907e4b4581
PhalgunaRao <phalu900@gmail.com>
2023-03-20 22:35:18 +0530

    my first iOS swift interact code

--- Bulleseye/ContentView.swift
+++ Bulleseye/ContentView.swift
@@ -8,14 +8,45 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var alertIsVisible:Bool = false
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            Text("🎯🎯🎯 \nPut the bullseye as close as you can to")
+                .bold()
+                .multilineTextAlignment(.center).lineSpacing(4.0)
+                .font(.footnote)
+                .kerning(/*@START_MENU_TOKEN@*/1.0/*@END_MENU_TOKEN@*/)
+            
+            Text("89")
+                .kerning(-1.0)
+                .font(.largeTitle)
+                .fontWeight(.black)
+            
+            HStack{
+                Text("1").bold()
+                Slider(value: .constant(50), in: 1.0...100.0)
+                Text("100").bold()
+                
+            }
+            Button("Hit me"){
+               alertIsVisible=true
+                
+            }
+            .alert(
+                "Hello there!",
+                isPresented: $alertIsVisible,
+                actions: {
+                    Button("Awesome"){
+                        print("Alert closed")
+                    }
+                },
+                message: {
+                   Text("This is my first alert!")
+                }
+                )
+
         }
-        .padding()
+                
     }
 }
 
